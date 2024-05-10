1af99012b4e0ace5952d3491bb19c65bd180fdca
Gabriel-Oliveira-Prado <gabrieloliveira.prado@hotmail.com>
2024-05-10 14:35:56 -0300

    primeiro

---
+++ telas/login/Login.py
@@ -0,0 +1,18 @@
+from kivymd.app import MDApp
+from kivy.lang import Builder
+from kivy.core.window import Window
+from kivymd.uix.screen import Screen
+from kivy.metrics import dp
+import os
+
+class App(MDApp):
+    def build(self):
+        # Definindo a resolução da janela para um tamanho típico de tela de celular
+        Window.size = (dp(360), dp(640))  # Largura x Altura em dp
+
+        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.primary_palette = "Gray"
+        return Builder.load_file(os.path.join("telas\login\login.kv"))
+
+if __name__ == "__main__":
+    App().run()
