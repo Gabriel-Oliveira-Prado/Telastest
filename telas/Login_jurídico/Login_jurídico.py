b58e2d6e4d297021f8a096629ef7647b688891b4
Gabriel-Oliveira-Prado <gabrieloliveira.prado@hotmail.com>
2024-05-17 23:21:51 -0300

    Novo

--- telas/Login_jurídico/Login_jurídico.py
+++ telas/Login_jurídico/Login_jurídico.py
@@ -14,7 +14,7 @@ class App(MDApp):
         # Definindo a resolução da janela para um tamanho típico de tela de celular
         Window.size = (dp(360), dp(640))  # Largura x Altura em dp
 
-        self.theme_cls.theme_style = "Light"
+        self.theme_cls.theme_style = "Dark"
         self.theme_cls.primary_palette = "Gray"
         return Builder.load_file(os.path.join("telas\Login_jurídico\Login_jurídico.kv"))
 
